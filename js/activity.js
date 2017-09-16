140263ae8b15910487251810e3c4da768ac3bd1c
loloof64 <laurent bernabe>
2017-09-16 13:42:24 +0200

    Starting to work with Konva library.

--- js/activity.js
+++ js/activity.js
@@ -1,11 +1,33 @@
 define(["sugar-web/activity/activity"], function (activity) {
 
 	// Manipulate the DOM only when it is ready.
-	require(['domReady!'], function (doc) {
+	require(['domReady!', 'konva'], function (doc) {
 
 		// Initialize the activity.
 		activity.setup();
 
+		var toolbarHeight = 0;
+
+		var stage = new Konva.Stage({
+			container: 'canvas',
+			width: window.innerWidth,
+			height: window.innerHeight - toolbarHeight
+		});
+
+		var layer = new Konva.Layer();
+
+		var circle = new Konva.Circle({
+			x: stage.getWidth() / 2,
+			y: stage.getHeight() / 2,
+			radius: 70,
+			fill: 'red',
+			stroke: 'black',
+			strokeWidth: 4
+		});
+
+		layer.add(circle);
+
+		stage.add(layer);
 	});
 
 });
