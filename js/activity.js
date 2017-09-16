771e9f9f641c1972216086776fb9bee725e03ad9
loloof64 <laurent bernabe>
2017-09-16 17:23:10 +0200

    Added balloon builder.

--- js/activity.js
+++ js/activity.js
@@ -1,25 +1,41 @@
 define(["sugar-web/activity/activity"], function (activity) {
 
 	// Manipulate the DOM only when it is ready.
-	require(['domReady!', 'konva', 'class'], function (doc) {
+	require(['domReady!', 'konva', 'oolib'], function (doc) {
 
 		// Initialize the activity.
 		activity.setup();
 
 		var Canvas = buildCanvasClass();
+		var BaloonBuilder = buildBalloonBuilder();
+
 		var theCanvas = new Canvas();
+		var baloon1 = new BaloonBuilder()
+			.setLocation(40, 90)
+			.setRadius(90)
+			.setText('5+8')
+			.setBackgroundColor('#FC0000')
+			.setForegroundColor('#003CFD')
+			.setTextSizeRatio(0.8)
+			.setTextXRatio(0.35)
+			.setTextYRatio(0.55)
+			.build();
 
-		var circle = new Konva.Circle({
-			x: theCanvas.stage.getWidth() / 2,
-			y: theCanvas.stage.getHeight() / 2,
-			radius: 70,
-			fill: 'red',
-			stroke: 'black',
-			strokeWidth: 4
-		});
+		var baloon2 = new BaloonBuilder()
+			.setLocation(200, 80)
+			.setRadius(240)
+			.setText('15+72')
+			.setBackgroundColor('#00DF27')
+			.setForegroundColor('black')
+			.setTextSizeRatio(0.6)
+			.setTextXRatio(0.15)
+			.setTextYRatio(0.70)
+			.build();
 
-		theCanvas.layer.add(circle);
-		theCanvas.layer.draw();
+		theCanvas.getLayer().add(baloon1);
+		theCanvas.getLayer().add(baloon2);
+
+		theCanvas.getLayer().draw();
 	});
 
 });
--- js/canvas_setup.js
+++ js/canvas_setup.js
@@ -21,12 +21,107 @@ function buildCanvasClass() {
         return stage;
     }
 
-    return new Class({
-        initialize: function () {
+    return oo.createClass({
+        _create: function () {
             this.stage = buildCanvasStage();
             this.layer = new Konva.Layer();
 
             this.stage.add(this.layer);
+        },
+
+        getStage: function () {
+            return this.stage;
+        },
+
+        getLayer: function () {
+            return this.layer;
+        }
+    });
+}
+
+function buildBalloonBuilder() {
+    return oo.createClass({
+        _create: function () {
+            this.x = 0;
+            this.y = 0;
+            this.radius = 0;
+            this.text = "";
+            this.backgroundColor = 'white';
+            this.foregroundColor = 'black';
+            this.textSizeRatio = 0.1; // ratio textSize / radius
+            this.textXRatio = 0.1; // ratio textX / radius
+            this.textYRatio = 0.1; // ratio textY / radius
+        },
+
+        setLocation: function (x, y) {
+            this.x = x;
+            this.y = y;
+            return this;
+        },
+
+        setRadius: function (radius) {
+            this.radius = radius;
+            return this;
+        },
+
+        setText: function (text) {
+            this.text = text;
+            return this;
+        },
+
+        setBackgroundColor: function (backgroundColor) {
+            this.backgroundColor = backgroundColor;
+            return this;
+        },
+
+        setForegroundColor: function (foregroundColor) {
+            this.foregroundColor = foregroundColor;
+            return this;
+        },
+
+        // ratio textSize / radius
+        setTextSizeRatio(ratio) {
+            this.textSizeRatio = ratio;
+            return this;
+        },
+
+        // ratio textX / radius
+        setTextXRatio(ratio) {
+            this.textXRatio = ratio;
+            return this;
+        },
+
+        // ratio textY / radius
+        setTextYRatio(ratio) {
+            this.textYRatio = ratio;
+            return this;
+        },
+
+        build: function () {
+            var group = new Konva.Group({
+                x: this.x,
+                y: this.y
+            });
+
+            var circle = new Konva.Circle({
+                x: this.radius,
+                y: this.radius,
+                radius: this.radius,
+                fill: this.backgroundColor
+            });
+
+            var text = new Konva.Text({
+                x: parseInt(this.radius * this.textXRatio),
+                y: parseInt(this.radius * this.textYRatio),
+                text: this.text,
+                fontSize: parseInt(this.radius * this.textSizeRatio),
+                fill: this.foregroundColor
+            });
+
+            group.add(circle);
+            group.add(text);
+
+            return group;
         }
     });
 }
